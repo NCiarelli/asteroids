d294344183cb89f300a84e2d810135e5a31b17be
NCiarelli <nicholas.ciarelli@gmail.com>
2024-08-24 09:16:23 -0400

    Ch 2-2: Added a game loop of filling a black screen

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    (numpass, numfail) = pygame.init()
+    print(f"{numpass} pygame modules initialized successfully")
+    print(f"{numfail} pygame modules failed to initialize")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
